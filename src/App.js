1f4643aa642527c9199820a20b891ff439f07a4b
TUSHYA TRIVEDI <ttushya@gamil.com>
2026-04-02 22:30:42 +0530

    added state management to add taks, mark as completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import { FaCheck } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
+import { useState } from "react";
 
 const itemArr = [
-  { text: "item1", count: 0, checked: false },
-  { text: "item2", count: 0, checked: false },
-  { text: "item3", count: 0, checked: false },
-  { text: "item4", count: 0, checked: false },
+  { id: crypto.randomUUID(), text: "item1", count: 0, checked: false },
+  { id: crypto.randomUUID(), text: "item2", count: 0, checked: false },
+  { id: crypto.randomUUID(), text: "item3", count: 0, checked: false },
+  { id: crypto.randomUUID(), text: "item4", count: 0, checked: true },
 ];
 export default function App() {
+  const [items, setItems] = useState(itemArr);
+  const total = items.length;
+  const packed = items.filter((x) => x.checked).length;
+
+  function handleAddItem(item) {
+    setItems([...items, { ...item, id: crypto.randomUUID() }]);
+  }
+
+  function handleChange(item) {
+    let list = items.map((x) => {
+      if (x.id === item.id) return item;
+      else {
+        return x;
+      }
+    });
+    setItems(list);
+  }
+
   return (
     <div className="container">
       <Logo />
-      <Form />
-      <PackingList items={itemArr} />
-      <Stats />
+      <Form onAdd={handleAddItem} />
+      <PackingList items={items} onChange={handleChange} />
+      <Stats total={total} packed={packed} />
     </div>
   );
 }
@@ -22,7 +41,8 @@ function Logo() {
   return <div className="logo">Far Away</div>;
 }
 
-function Form() {
+function Form({ onAdd }) {
+  const [data, setData] = useState({ text: "", count: 1, checked: false });
   let optionItems = Array(20)
     .fill(0)
     .map((x, index) => index + 1);
@@ -30,21 +50,37 @@ function Form() {
   return (
     <div className="form">
       <p>What do you need for the trip?</p>
-      <select name="items">
+      <select
+        name="items"
+        onChange={(e) => {
+          setData({ ...data, count: Number(e.target.value) });
+        }}
+      >
         {optionItems.map((x) => {
-          return <option value={x}>{x}</option>;
+          return (
+            <option key={x} value={x}>
+              {x}
+            </option>
+          );
         })}
       </select>
-      <input type="text" value={""} placeholder="item name" />
-      <button>Add</button>
+      <input
+        type="text"
+        value={data.text}
+        onChange={(e) => {
+          setData({ ...data, text: e.target.value });
+        }}
+        placeholder="item name"
+      />
+      <button onClick={() => onAdd(data)}>Add</button>
     </div>
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onChange }) {
   return (
     <div className="packing-list">
-      <ItemList items={items} />
+      <ItemList items={items} onChange={onChange} />
       <ListFunctions />
     </div>
   );
@@ -63,16 +99,24 @@ function ListFunctions() {
   );
 }
 
-function ItemList({ items }) {
-  let list = items.map((x) => <Item item={x} />);
+function ItemList({ items, onChange }) {
+  let list = items.map((x) => (
+    <Item key={x.text} onChange={onChange} item={x} />
+  ));
   return <div className="items">{list}</div>;
 }
 
-function Item({ item }) {
+function Item({ item, onChange }) {
   return (
     <div className="item">
-      <input type="checkbox" value={item.checked} />
-      <p>{item.text}</p>
+      <input
+        type="checkbox"
+        checked={item.checked}
+        onChange={(e) => {
+          onChange({ ...item, checked: e.target.checked });
+        }}
+      />
+      <p>{` ${item.count} ${item.text}`}</p>
       {item.checked ? (
         <FaCheck className="check" />
       ) : (
@@ -86,8 +130,7 @@ function Stats({ total, packed }) {
   return (
     <div className="stats">
       <p>
-        You have {total} items in your list, you already packed {packed}{" "}
-        {`(${(packed / total) * 100})`}
+        You have {total} items in your list, you already packed {packed}
       </p>
     </div>
   );
